d0ac64c0c7135e2653c337c435f558b96d6ee678
Brayan Alejandro Garcia Perez <xoltex_new@xoltex.com>
2025-06-11 21:27:05 -0600

    se suben cambios de el pipeline

--- lbd-xoltex-generar-py-llaves/lambda_function.py
+++ lbd-xoltex-generar-py-llaves/lambda_function.py
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
         raise AppError(404, "Parametros no validos")
         
     
-    path_params = event.get("pathParameters", {})
+    path_params = event.get("pathParameters", {}) 
     response= funcion.crearLLaves(path_params,event)
     return JSONResponse(content=AppResponse.ok(response), status_code=200)
 
